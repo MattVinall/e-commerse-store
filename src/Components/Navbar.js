c438d60d28f730403af5dec9502eccb7e07cb96b
Matt Vinall <mattvinall@Matts-MacBook-Pro.local>
2019-01-18 22:13:47 -0500

    set up nav bar and some inital stylings using bootsrap

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg'
+import styled from 'styled-components'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ButtonContainer } from './Button'
 
 
-
-class Navbar extends Component {
+export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 {/* 
                     https://www.iconfinder.com/icons/1243689/call_phone_iconCreative Commons (Attribution 3.0 Unported);
                     https://www.iconfinder.com/Makoto_msk */ }
@@ -22,14 +24,23 @@ class Navbar extends Component {
                     </li>
                 </ul>
                 <Link to='/cart' className="ml-auto">
-                    <button>
-                        <i className="fas fa-cart-plus" />
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <i className="fas fa-cart-plus" />
+                        </span>
                         my cart
-                    </button>
+                    </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
 
-export default Navbar;
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link {
+        color: var(--mainWhite)!important;
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
+`

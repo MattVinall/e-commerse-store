beef87d030d676e2437a0307f783374ac13c9bb0
Matt Vinall <mattvinall@Matts-MacBook-Pro.local>
2019-01-19 10:26:46 -0500

    got products on page and Link set up to details page

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../context';
 
 class Product extends Component {
     render() {
+        const { id, title, img, price, inCart } = this.props.product;
         return (
-            <div>
-                <h3>Hello from Product</h3>
-            </div>
+            <ProductWrapper className="col-9 mx-auto cold-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5" onClick={console.log('you clicked me')}>
+                        <Link to="/details">
+                            <img src={img} alt="product" className="card-img-top" />
+                        </Link>
+                    </div>
+                </div>
+            </ProductWrapper>
         )
     }
 }
 
 export default Product;
+
+const ProductWrapper = styled.div`
+
+`

1ab2204e3ad479759231b1cf77f2d43320a3be15
Matt Vinall <mattvinall@Matts-MacBook-Pro.local>
2019-01-25 16:12:26 -0500

    added cartTotals component and set up functionality and user is able to remove product from the cart

--- src/context.js
+++ src/context.js
@@ -63,7 +63,7 @@ class ProductProvider extends Component {
             return { products: tempProducts, cart: [...this.state.cart, product] }
         },
             () => {
-                console.log(this.state)
+                this.addTotals();
             }
         );
     };
@@ -78,24 +78,66 @@ class ProductProvider extends Component {
     closeModal = () => {
         this.setState(() => {
             return { modalOpen: false }
-        })
-    }
+        });
+    };
 
     increment = id => {
         console.log('this is increment method');
-    }
+    };
 
     decrement = id => {
         console.log('this is decrement method');
-    }
+    };
 
     removeItem = id => {
-        console.log('item removed');
-    }
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id)
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(
+            () => {
+                return {
+                    cart: [...tempCart],
+                    products: [...tempProducts]
+                }
+            }, () => {
+                this.addTotals();
+            });
+    };
 
     clearCart = () => {
-        console.log('cart was cleared');
-    }
+        this.setState(
+            () => {
+                return { cart: [] }
+            }, () => {
+                this.setProducts();
+                this.addTotals();
+            });
+    };
+
+    addTotals = () => {
+        let subTotal = 0;
+        this.state.cart.map(item => (subTotal += item.total));
+        const tempTax = subTotal * 0.13;
+        const tax = parseFloat(tempTax.toFixed(2));
+        const total = subTotal + tax;
+        this.setState(
+            () => {
+                return {
+                    cartSubTotal: subTotal,
+                    cartTax: tax,
+                    cartTotal: total
+                }
+            });
+    };
 
     render() {
         return (
@@ -113,7 +155,7 @@ class ProductProvider extends Component {
                 {this.props.children}
             </ProductContext.Provider>
         )
-    }
+    };
 }
 
 const ProductConsumer = ProductContext.Consumer;
